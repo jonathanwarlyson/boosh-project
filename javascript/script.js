6cdb8bbb386e0c1819cda753c656c60cd22e123b
Jonathan Warlyson <130255448+jonathanwarlyson@users.noreply.github.com>
2023-04-27 23:14:17 -0300

    Delete javascript directory

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-function showModal() {
-    var element = document.getElementById("modal");
-    element.classList.add("show-modal");
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    showModal();
-});
